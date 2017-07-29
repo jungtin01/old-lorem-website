7951746eee076203efb59a30b35c02f06bc602e8
Jakcalie <nguyentrungthinh2000@gmail.com>
2017-07-29 10:19:35 +0700

    update lần 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 	$("#leftContent .row:first").css('margin-top','30px');
+	$("#day").on('click',function(){
+		$('#rankingDay').fadeIn();
+		$('#rankingMonth').fadeOut();
+		$('#rankingYear').fadeOut();
+	});
+	$("#month").on('click',function(){
+		$('#rankingDay').fadeOut();
+		$('#rankingMonth').fadeIn();
+		$('#rankingYear').fadeOut();
+	});
+	$("#year").on('click',function(){
+		$('#rankingDay').fadeOut();
+		$('#rankingMonth').fadeOut();
+		$('#rankingYear').fadeIn();
+	});
 });
