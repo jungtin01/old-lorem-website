6ff30cd127245fb3f549d3ee832e890b82542275
Nguyễn Trung Thinh <nguyentrungthinh2000@gmail.com>
2017-07-30 12:35:11 +0700

    update lần 5

---
+++ assets/js/post.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+
+});
